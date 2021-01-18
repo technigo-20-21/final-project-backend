3647b80db7dfc2bc9a69c3777d34f318907fbbc4
petrame <63057558+petrame@users.noreply.github.com>
2021-01-18 16:09:46 +0100

    backend created from express-api-starter

--- server.js
+++ server.js
@@ -1,6 +1,52 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/torslandalocals';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [2, 'Use a minimum of 2 characters'],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: [6, 'The password must be at least six characters long.'],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+    unique: true,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  // If the password is not changed
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  // Encrypt the password if it is changed
+  const salt = bcrypt.genSaltSync();
+  user.password = bcrypt.hashSync(user.password, salt);
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
